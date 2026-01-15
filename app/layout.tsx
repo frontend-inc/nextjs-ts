cf764d552068020ff6cab1c3786aadae587bb440
Rami Bitar <rbitar@gmail.com>
2026-01-15 09:25:23 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Frontend',
+  description: 'Start prompting',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="m-0 p-0 font-body">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1
+        className="text-6xl font-bold font-heading text-black text-center"
+      >
+        Start prompting
+      </h1>
+    </div>
+
+  );
+};
+
+export default Home;
